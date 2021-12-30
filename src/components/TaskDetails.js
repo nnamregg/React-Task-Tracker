b6bcbd38b2e242a8a0be63d4812d9dd977f6f12b
nnamregg <nnamreg.t@gmail.com>
2021-12-30 20:24:11 -0300

    Fixed memory leak on TaskDetails.js

---
+++ src/components/TaskDetails.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import Button from './Button'
+
+const TaskDetails = () => {
+	const [loading, setLoading] = useState(true)
+	const [task, setTask] = useState({})
+	// const [error, setError] = useState(null)
+
+	const params = useParams()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		let isSubscribed = true
+		const fetchTask = async () => {
+			const res = await fetch(`http://localhost:5000/tasks/${params.id}`)
+			const data = await res.json()
+
+			if (res.status === 404) {
+				navigate('/')
+			}
+
+			if (isSubscribed) {
+				setTask(data)
+				setLoading(false)
+			}
+		}
+		
+		fetchTask()
+
+		return () => isSubscribed = false
+	})
+
+	// if (error) {
+	// 	return <Navigate to='/' />
+	// }
+
+	return loading ? (
+		<h3>Loading...</h3>
+	) : (
+		<div>
+			<h3>{task.text}</h3>
+			<p>{task.day}</p>
+			<Button
+				onClick={() => {
+					navigate(-1)
+				}}
+				text='Go back' />
+		</div>
+	)
+}
+
+export default TaskDetails
